061d003d1c0e0d1c54a2d134e8c18ae9575ddef3
Riley Bautista <rbautista@duck.com>
2025-08-10 07:35:15 -0400

    Added orbs
    In car rn with no data, will push once I have wifi probably in 2 hours

--- killzone.gd
+++ killzone.gd
@@ -3,7 +3,6 @@ extends Area2D
 @onready var timer = $Timer
 @onready var player = $"../Player"
 @onready var playerSprite = $"../Player/AnimatedSprite2D"
-@onready var deathLogger = $"../Log"
 # Called when the node enters the scene tree for the first time.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,8 +13,10 @@ func _on_body_entered(body: Node2D) -> void:
 	if body == player:
 		timer.start()
 		playerSprite.play("death")
-		deathLogger.deaths = 1
+		Log.deaths = 1 + Log.deaths
+		print(Log.deaths)
 		player.stop = true
 		
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
+	
--- log.gd
+++ log.gd
@@ -1,4 +1,4 @@
 extends Node2D
 
-@export var deaths = 0 
+@export var deaths = 0
 @export var orbsCollected = 0 
