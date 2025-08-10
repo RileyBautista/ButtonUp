3ad59ea0607cf532d39c5f234fe3c477742d0e54
Riley Bautista <rbautista@duck.com>
2025-08-10 07:53:06 -0400

    THE ORBS WORK NOW
    👽

--- orb.gd
+++ orb.gd
@@ -1,11 +1,21 @@
 extends Area2D
 
+@onready var player = $"../Player"
+@onready var orbSprite = $AnimatedSprite2D
+@onready var timer = $Timer
+	
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+
+func _on_body_entered(body: Node2D) -> void:
+	if body == player:
+		Log.orbsCollected += 1 
+		orbSprite.play("collected")
+		set_deferred("monitoring", false)
+		timer.start()
+		print(Log.orbsCollected)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+
+func _on_timer_timeout() -> void:
+	visible = false
