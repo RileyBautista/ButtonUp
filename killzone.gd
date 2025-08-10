fe912bee0822d86d24ac61bc5eb47104035a0eaf
Riley Bautista <rbautista@duck.com>
2025-08-10 01:27:12 -0400

    AAAAAAAAAAAAAAAAAAAAAAA
    should i pull an all-nighter..

--- killzone.gd
+++ killzone.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var timer = $Timer
 @onready var player = $"../Player"
 @onready var playerSprite = $"../Player/AnimatedSprite2D"
-
+@onready var deathLogger = $"../Log"
 # Called when the node enters the scene tree for the first time.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,6 +14,8 @@ func _on_body_entered(body: Node2D) -> void:
 	if body == player:
 		timer.start()
 		playerSprite.play("death")
+		deathLogger.deaths = 1
 		player.stop = true
+		
 func _on_timer_timeout() -> void:
 	get_tree().reload_current_scene()
---
+++ theOhGloriousLogScene.gd
@@ -0,0 +1,4 @@
+# I don't know how the heck you log data so LETS TRY USING THISSSSSSSSSSSS
+extends Node2D
+@export var deaths = 0 
+@export var orbsCollected = 0 
